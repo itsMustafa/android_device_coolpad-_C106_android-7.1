3c09e2230e5938dbc651d861a89dfbcceef34284
Mustafa Shaikh <smustfa147@gmail.com>
2018-05-05 15:24:45 +0530

    Update full_c103.mk

--- full_c103.mk
+++ full_c103.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from device.mk
-$(call inherit-product, device/leeco/c103/device.mk)
+$(call inherit-product, device/coolpad/c103/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_c103
